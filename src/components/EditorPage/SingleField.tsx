9ab8a998211edfa1c63d9ec693dd2e5679da3f83
kacper-echaust <k9echaust@gmail.com>
2025-12-09 20:08:50 +0100

    create single field

---
+++ src/components/EditorPage/SingleField.tsx
@@ -0,0 +1,30 @@
+import { useDraggable } from '@dnd-kit/core'
+import type { Field } from '../../types'
+import { Button } from '@chakra-ui/react/button'
+
+const SingleField = ({ field }: { field: Field }) => {
+  const { setNodeRef, listeners, attributes, transform } = useDraggable({
+    id: field.id,
+    data: field,
+  })
+
+  return (
+    <Button
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      size="sm"
+      variant="outline"
+      style={{
+        cursor: 'grab',
+        transform: transform
+          ? `translate(${transform.x}px, ${transform.y}px)`
+          : undefined,
+      }}
+    >
+      {field.label}
+    </Button>
+  )
+}
+
+export { SingleField }
