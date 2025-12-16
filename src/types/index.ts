290a204e82b54c88bf4466d8547aa4bbbcfc1cb4
kacper-echaust <k9echaust@gmail.com>
2025-12-16 19:15:16 +0100

    add some types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,31 @@
 export type Field = {
   id: string
   type: string
-  label: string
   required: boolean
+  name: string
 }
-export type CanvasFieldsType = {
+export type CanvasFieldsType = FormType & {
   id: string
-  label: string
+  name: string
   type: string
   uid: string
+  isEdit: boolean
 }
+export type FormType = {
+  label: string
+  placeholder?: string | undefined
+  required: boolean
+  minLength?: string
+  maxLength?: string
+  options?: SelectOption[]
+}
+export type FieldSettings = {
+  onChange: (field: string, value: FieldValue) => void
+  formValues: FormType
+}
+export type SelectOption = {
+  id: string
+  optionName: string
+}
+
+export type FieldValue = string | boolean | SelectOption[]
