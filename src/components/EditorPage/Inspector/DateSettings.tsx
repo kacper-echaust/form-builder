94d8c43fb0876f21bbb8713be41db577d4c97396
kacper-echaust <k9echaust@gmail.com>
2026-01-06 17:05:47 +0100

    add default value to date component

--- src/components/EditorPage/Inspector/DateSettings.tsx
+++ src/components/EditorPage/Inspector/DateSettings.tsx
@@ -2,13 +2,14 @@ import { Checkbox, Field, Input } from '@chakra-ui/react'
 import type { FieldSettings } from '../../../types'
 import { useState, type ChangeEvent } from 'react'
 
-const DateSettings = ({ onChange, formValues }: FieldSettings) => {
+const DateSettings = ({ onChange, formValues, errors }: FieldSettings) => {
   const { fromTo } = formValues
   const [isChecked, setIsChecked] = useState(fromTo ? true : false)
 
   const handleChange = (name: string, e: ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value)
-    const updated = { from: fromTo?.from || '', to: fromTo?.to || '' }
+    if (!isChecked) return
+    const dateNow = new Date().toISOString().slice(0, 10)
+    const updated = { from: fromTo?.from || dateNow, to: fromTo?.to || dateNow }
     onChange('fromTo', { ...updated, [name]: e.target.value })
   }
 
@@ -24,13 +25,23 @@ const DateSettings = ({ onChange, formValues }: FieldSettings) => {
       </Checkbox.Root>
       {isChecked && (
         <>
-          <Field.Root>
+          <Field.Root invalid={!!errors.fromTo}>
             <Field.Label>From</Field.Label>
-            <Input type="date" onChange={(e) => handleChange('from', e)} />
+            <Input
+              type="date"
+              onChange={(e) => handleChange('from', e)}
+              defaultValue={new Date().toISOString().slice(0, 10)}
+            />
+            <Field.ErrorText>{errors.fromTo}</Field.ErrorText>
           </Field.Root>
-          <Field.Root>
+          <Field.Root invalid={!!errors.fromTo}>
             <Field.Label>To</Field.Label>
-            <Input type="date" onChange={(e) => handleChange('to', e)} />
+            <Input
+              type="date"
+              onChange={(e) => handleChange('to', e)}
+              defaultValue={new Date().toISOString().slice(0, 10)}
+            />
+            <Field.ErrorText>{errors.fromTo}</Field.ErrorText>
           </Field.Root>
         </>
       )}
