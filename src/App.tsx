7823b0c60bd5ab059f539aa0162844ac5f642166
kacper-echaust <k9echaust@gmail.com>
2026-01-10 08:03:31 +0100

    create form prewiev and add button in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
 import { EditorPage } from './components/EditorPage/EditorPage'
+import { FormPreview } from './components/FormPreview/FormPreview'
 import { CanvasFieldsProvider } from './context/CanvasFieldsContext'
+import { Button } from '@chakra-ui/react'
 
 const App = () => {
+  const [preview, setPreview] = useState(false)
   return (
     <CanvasFieldsProvider>
-      <EditorPage />
+      <Button
+        onClick={() => setPreview(!preview)}
+        position="absolute"
+        top={5}
+        left={5}
+      >
+        {preview ? 'Go to edit' : 'Live preview'}
+      </Button>
+      {!preview ? <EditorPage /> : <FormPreview />}
     </CanvasFieldsProvider>
   )
 }
