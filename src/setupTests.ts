bc981f28a08d799f9110407c5d6faad25ca0f799
kacper-echaust <k9echaust@gmail.com>
2026-02-13 17:47:35 +0100

    install and setup rtl jest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,14 @@
 import '@testing-library/jest-dom/vitest'
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    dispatchEvent: vi.fn(),
+  })),
+})
